ab141772bfd2ac60b28b236eb2a90efdee9fd135
Piotr Januszewski <piojanuszewski@gmail.com>
2019-10-31 16:15:42 +0100

    Add missing dependencies

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=['cryptography>=2.2.2', 'PyYAML', 'fabric3', 'path.py', 'jinja2', 'six', 'attrs>=17.3', 'click',
-                      'docker', 'kubernetes>=5.0.0', 'google-cloud', 'termcolor', 'pyperclip'],
+                      'docker', 'kubernetes>=5.0.0', 'google-cloud', 'termcolor', 'pyperclip',
+                      'cloudpickle', 'neptune-client', 'munch', 'gin-config'],
     entry_points={
         'console_scripts': [
             'mrunner=mrunner.cli.mrunner_cli:cli'

6bae616991e7462225e512563a073eb82ac124f7
pmilos <pmilos@mimuw.edu.pl>
2020-01-18 09:49:37 +0100

    evnironmental variables fix

--- examples/experiment_basic_conf.py
+++ examples/experiment_basic_conf.py
@@ -4,14 +4,14 @@ from munch import Munch
 from mrunner.experiment import Experiment
 import os
 
-if "NEPTUNE_API_TOKEN" not in os.environ or "PROJECT_QUALIFIED_NAME" not in os.environ:
-    print("Please set NEPTUNE_API_TOKEN and PROJECT_QUALIFIED_NAME env variables")
+if "NEPTUNE_API_TOKEN" not in os.environ or "NEPTUNE_PROJECT_NAME" not in os.environ:
+    print("Please set NEPTUNE_API_TOKEN and NEPTUNE_PROJECT_NAME env variables")
     print("Their values can be from up.neptune.ml. Click help and then quickstart.")
     exit(1)
 
 exp = Experiment(name='Basic experiment',
                  script='python experiment_basic.py',
-                 project=os.environ["PROJECT_QUALIFIED_NAME"],
+                 project=os.environ["NEPTUNE_PROJECT_NAME"],
                  tags=['test_experiments'],
                  env={"NEPTUNE_API_TOKEN": os.environ["NEPTUNE_API_TOKEN"],
                       "PYTHON_PATH": '$PYTHONPATH:.'},
